2e86715b110502fea66108b96ec2fe9449769bb5
joozery <zerryboy28@gmail.com>
2026-05-11 23:38:49 +0700

    Switch to Resend for email service

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,23 +1,21 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
 const sendEmail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE, // e.g. Gmail
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
-    const mailOptions = {
-        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    const { data, error } = await resend.emails.send({
+        from: 'LostFound <onboarding@resend.dev>', // Default for unverified domains
         to: options.email,
         subject: options.subject,
-        text: options.message,
         html: options.html,
-    };
+    });
 
-    await transporter.sendMail(mailOptions);
+    if (error) {
+        console.error('Resend Error:', error);
+        throw new Error('Failed to send email via Resend');
+    }
+
+    return data;
 };
 
 module.exports = sendEmail;
